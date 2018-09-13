4e0c2c27c81e4e2b2015e736a49fb301dcc03dec
Matias-Barrios <soymatiasbarrios@gmail.com>
2018-09-13 13:56:49 -0300

    Commit inicial

---
+++ main.sh
@@ -0,0 +1,161 @@
+#!/bin/bash
+
+## Source a las dependencias
+
+source functions/colors.def
+
+## Definicion de pantallas
+
+function Main_menu() {
+    clear
+
+    echo "${MAGENTA}########################################################${NORMAL}"
+    echo "${GREEN} Bienvenido a Centos 7 ${NORMAL}"
+    printf "${CYAN} \t \t ¿Que desea hacer? ${NORMAL}\n"
+    echo "${MAGENTA}########################################################${NORMAL}"
+    echo "${YELLOW} 1 - ${NORMAL} Administracion de cuentas de usuario y grupos"
+    echo "${YELLOW} 2 - ${NORMAL} Administracion de Informix DBMS"
+    echo "${YELLOW} 3 - ${NORMAL} Monitoreo de Recuros"
+    echo "${YELLOW} 4 - ${NORMAL} Monitoreo de acceso al servidor"
+    echo "${YELLOW} 5 - ${NORMAL} Servicios"
+    echo "${YELLOW} q - ${NORMAL} SALIR"
+    read -s -n 1 option
+    case $option in
+        1) 
+            Administracion_de_cuentas_de_usuario
+            ;;
+        2) 
+            Administracion_de_Informix_DBMS
+            ;;
+        3) 
+            Monitoreo_de_Recuros
+            ;;
+        4) 
+            Monitoreo_de_acceso_al_servidor
+            ;;
+        5) 
+            Servicios
+            ;;
+        'q')
+            clear
+            exit 0
+            ;;
+        *)
+            Main_menu
+        ;;
+    esac
+}
+
+function Servicios() {
+    clear
+
+    echo "${MAGENTA}########################################################${NORMAL}"
+    echo "${GREEN} Servicios ${NORMAL}"
+    echo "${CYAN}########################################################${NORMAL}"
+    echo "${YELLOW} 1 - ${NORMAL} Ver estado de los servicios"
+    echo "${YELLOW} 2 - ${NORMAL} Parar o deshabilitar un servicio"
+    echo "${YELLOW} 1 - ${NORMAL} Iniciar o habilitar un servicio"
+    echo "${YELLOW} q - ${NORMAL} SALIR"
+    read -s -n 1 option
+    case $option in
+        'q')
+            Main_menu
+            ;;
+        *)
+            Servicios
+        ;;
+    esac
+    
+}
+
+function Monitoreo_de_acceso_al_servidor() {
+    clear
+
+    echo "${CYAN}########################################################${NORMAL}"
+    echo "${GREEN} Monitoreo de acceso al servidor ${NORMAL}"
+    echo "${CYAN}########################################################${NORMAL}"
+    echo "${YELLOW} 1 - ${NORMAL} Ver logs de acceso"
+    echo "${YELLOW} q - ${NORMAL} SALIR"
+    read -s -n 1 option
+    case $option in
+        'q')
+            Main_menu
+            ;;
+        *)
+            Monitoreo_de_acceso_al_servidor
+        ;;
+    esac
+    
+}
+
+function Monitoreo_de_Recuros() {
+    clear
+
+    echo "${CYAN}########################################################${NORMAL}"
+    echo "${GREEN} Monitoreo de Recuros ${NORMAL}"
+    echo "${CYAN}########################################################${NORMAL}"
+    echo "${YELLOW} 1 - ${NORMAL} Ver status de recursos del servidor"
+    echo "${YELLOW} q - ${NORMAL} SALIR"
+    read -s -n 1 option
+    case $option in
+        'q')
+            Main_menu
+            ;;
+        *)
+            Monitoreo_de_Recuros
+        ;;
+    esac
+    
+}
+
+function Administracion_de_Informix_DBMS() {
+    clear
+
+    echo "${CYAN}########################################################${NORMAL}"
+    echo "${GREEN} Administracion de Informix DBMS ${NORMAL}"
+    echo "${CYAN}########################################################${NORMAL}"
+    echo "${YELLOW} 1 - ${NORMAL} Ver status"
+    echo "${YELLOW} 2 - ${NORMAL} Parar"
+    echo "${YELLOW} 3 - ${NORMAL} Iniciar"
+    echo "${YELLOW} 4 - ${NORMAL} Baja Grupo"
+    echo "${YELLOW} 5 - ${NORMAL} Realizar respaldo AHORA"
+    echo "${YELLOW} q - ${NORMAL} SALIR"
+    read -s -n 1 option
+    case $option in
+        'q')
+            Main_menu
+            ;;
+        *)
+            Administracion_de_Informix_DBMS
+        ;;
+    esac
+    
+}
+
+function Administracion_de_cuentas_de_usuario() {
+    clear
+
+    echo "${CYAN}########################################################${NORMAL}"
+    echo "${GREEN} Administracion de cuentas de usuario y grupos ${NORMAL}"
+    echo "${CYAN}########################################################${NORMAL}"
+    echo "${YELLOW} 1 - ${NORMAL} Alta Usuario"
+    echo "${YELLOW} 2 - ${NORMAL} Alta Grupo"
+    echo "${YELLOW} 3 - ${NORMAL} Baja Usuario"
+    echo "${YELLOW} 4 - ${NORMAL} Baja Grupo"
+    echo "${YELLOW} 5 - ${NORMAL} Modificacion Usuario"
+    echo "${YELLOW} 6 - ${NORMAL} Modificacion Grupos"
+    echo "${YELLOW} q - ${NORMAL} SALIR"
+    read -s -n 1 option
+    case $option in
+        'q')
+            Main_menu
+            ;;
+        *)
+            Administracion_de_cuentas_de_usuario
+        ;;
+    esac
+    
+}
+
+## Ejecutar!
+Main_menu
